baa5798b1d674505c69a7944b059cedafb422c95
Aditya Ingale <adityaingale1814@gmail.com>
2024-07-15 09:39:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,48 @@
+
+
+
+// next.config.js
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+      },
+      {
+        source: '/skills',
+        destination: '/#skills',
+      },
+      {
+        source: '/experience',
+        destination: '/#experience',
+      },
+      {
+        source: '/education',
+        destination: '/#education',
+      },
+      {
+        source: '/projects',
+        destination: '/#projects',
+      },
+      {
+        source: '/contact',
+        destination: '/#contact',
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
+
